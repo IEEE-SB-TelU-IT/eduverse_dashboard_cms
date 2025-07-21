3ddc480186b601b8c5d4450ece2e3611b1d0d82d
Facalder <faainamacaldera16@gmail.com>
2025-07-21 13:14:28 +0700

    feat: bootstrap project and first commit

---
+++ app/layout/root-layout.tsx
@@ -0,0 +1,35 @@
+import { Links, Meta, ScrollRestoration, Scripts } from 'react-router'
+
+import type { Route } from '../+types/root'
+import '@/css/app.css'
+
+export function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
+
+export const links: Route.LinksFunction = () => [
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+  {
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
+    crossOrigin: 'anonymous',
+  },
+  {
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Geist:wght@100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap',
+  },
+]
